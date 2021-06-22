8cb9846f630de4a4f733bff7586ca5a2830b1627
Xiong <18771026891@163.com>
2021-06-22 23:28:24 +0800

    1

--- managementController/src/main/java/com/Xiong/controller/GitTest.java
+++ managementController/src/main/java/com/Xiong/controller/GitTest.java
@@ -2,7 +2,6 @@ package com.Xiong.controller;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("gitee");
-        System.out.println("ddd");
+        System.out.println("branch1");
     }
 }
